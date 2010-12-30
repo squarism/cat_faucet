457fccd91b3350a866eaaac286a4c0210e5fb380
Chris Dillon <milkfilk@gmail.com>
2010-12-30 12:38:49 -0500

    Set sinks index as home page.

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Smarthome::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "welcome#index"
+  root :controller => 'sinks', :action => 'index'
 
   # See how all your routes lay out with "rake routes"
 
